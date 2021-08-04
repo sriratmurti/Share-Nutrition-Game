aea5354fa9de1d6ebde222506f4b7d9d29c40bc5
Sri Ratmurti <85623746+sriratmurti@users.noreply.github.com>
2021-08-04 15:05:31 +0700

    new file pop up

---
+++ shareNutritions2/PopUp.swift
@@ -0,0 +1,16 @@
+//
+//  PopUp.swift
+//  shareNutritions2
+//
+//  Created by Sri Endah Ramurti on 04/08/21.
+//
+
+import SpriteKit
+import GameplayKit
+
+
+class popUpScene: SKScene {
+    
+    let brain = SKSpriteNode (imageNamed: "Brain Happy 1")
+    let kidney = SKSpriteNode (imageNamed: "")
+}
